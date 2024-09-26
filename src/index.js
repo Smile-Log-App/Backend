554a199998c4aacb53a2bbda76fa21384dbe4c5b
huipadyam <cekim3197@ewhain.net>
2024-09-26 19:52:56 +0900

    Chore: 유저 관련 라우트 추가 on index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import cors from 'cors'; // CORS 설정 1
 import authRoutes from './routes/authRoutes.js';
 import dailyRoutes from './routes/dailyRoutes.js';
 import monthlyRoutes from './routes/monthlyRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 
 // CORS 설정 2
 app.use(cors({
   origin: 'http://localhost:3000', // 허용할 프론트엔드 도메인
-  // methods: ['GET', 'POST', 'PUT', 'DELETE'], // 허용할 HTTP 메서드
+  // methods: ['GET', 'POST', 'PUT', 'DELETE'], a// 허용할 HTTP 메서드
   // allowedHeaders: ['Content-Type', 'Authorization'], // 허용할 헤더
   credentials: true // 인증 정보(쿠키 등)를 요청에 포함할 수 있도록 설정
 }));
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);  // 로그인 및 토큰 갱신 라우트
 app.use('/api', dailyRoutes);
 app.use('/api', monthlyRoutes);
+app.use('/api/user', userRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
